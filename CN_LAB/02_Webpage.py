71f5e26cd9f3087bd48e88bbd2de01cc218de4a2
Balachandar <bbalachandar4011@gmail.com>
2023-01-24 17:18:30 +0530

    added 02_Webpage.py

---
+++ CN_LAB/02_Webpage.py
@@ -0,0 +1,21 @@
+import socket
+import sys
+try:
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    print("Socket Succesfully created")
+except socket.error as err:
+    print("Socket creation failed with error %s" %(err))
+port = 80
+
+try:
+    host_ip = socket.gethostbyname('www.google.com')
+except socket.gaierror:
+ 
+    # this means could not resolve the host
+    print ("there was an error resolving the host")
+    sys.exit()
+
+s.connect((host_ip,port))
+
+s.sendall(b"GET/HTTP/1.0\r\nHost:www.youtube.com\r\nAccept:text/html\r\n\r\n")
+print(str(s.recv(4096),'utf-8'))
