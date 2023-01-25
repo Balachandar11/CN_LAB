504b0d3c3af72b97813db25d8116d94a9d589f95
Balachandar <bbalachandar4011@gmail.com>
2023-01-25 16:09:55 +0530

    modified 02_webpage.py

--- CN_LAB/02-Webpage_download_TCP/02_Webpage.py
+++ CN_LAB/02-Webpage_download_TCP/02_Webpage.py
@@ -11,12 +11,10 @@ hname = input("Enter host address(www.examgle.com):")
 try:
     host_ip = socket.gethostbyname(hname)
 except socket.gaierror:
- 
     # this means could not resolve the host
     print ("there was an error resolving the host")
     sys.exit()
+s.connect((hname,port))
 
-s.connect((host_ip,port))
-
-s.sendall(b"GET/HTTP/1.0\r\nHost:www.youtube.com\r\nAccept:text/html\r\n\r\n")
-print(str(s.recv(4096),'utf-8'))
+s.send(b"GET / HTTP/1.1\r\nHost:%b\r\n\r\n"%(hname.encode()))
+print(s.recv(4096).decode())
